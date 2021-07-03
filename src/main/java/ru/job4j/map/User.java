db63e99e8af92dc5f929d332c80546460636d1c6
CyberfuzZ <keepthelight@icloud.com>
2021-07-03 19:40:15 +0300

    Map - User

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -39,18 +39,4 @@ public class User {
                 + ", children=" + children
                 + '}';
     }
-
-    public static void main(String[] args) {
-        Calendar birthday = new GregorianCalendar(
-                1982, Calendar.APRIL, 2);
-        User user1 = new User("John", 0, birthday);
-        User user2 = new User("John", 0, birthday);
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
-        map.put(user2, new Object());
-        System.out.println(map);
-        System.out.println();
-        System.out.println(user1.hashCode());
-        System.out.println(user2.hashCode());
-    }
 }
