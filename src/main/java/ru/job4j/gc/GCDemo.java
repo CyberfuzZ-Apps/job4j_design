7ea0f2cc4535533a3a2da8aaafe24c766908f85c
CyberfuzZ <keepthelight@icloud.com>
2021-08-26 13:41:03 +0300

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/GCDemo.java
+++ src/main/java/ru/job4j/gc/GCDemo.java
@@ -1,29 +1,24 @@
 package ru.job4j.gc;
 
 public class GCDemo {
-    private static final long KB = 1000;
-    private static final long MB = KB * KB;
+    private static final long KB = 1024;
     private static final Runtime ENVIRONMENT = Runtime.getRuntime();
 
     public static void info() {
-        final long freeMemory = ENVIRONMENT.freeMemory();
-        final long totalMemory = ENVIRONMENT.totalMemory();
-        final long maxMemory = ENVIRONMENT.maxMemory();
+        final long freeMemory = ENVIRONMENT.freeMemory() / KB;
+        final long totalMemory = ENVIRONMENT.totalMemory() / KB;
+        final long maxMemory = ENVIRONMENT.maxMemory() / KB;
         System.out.println("=== Environment state ===");
-        System.out.printf("Free: %d%n", freeMemory / MB);
-        System.out.printf("Total: %d%n", totalMemory / MB);
-        System.out.printf("Max: %d%n", maxMemory / MB);
+        System.out.printf("Free: %d%n", freeMemory);
+        System.out.printf("Total: %d%n", totalMemory);
+        System.out.printf("Max: %d%n", maxMemory);
     }
 
     public static void main(String[] args) {
         info();
-//        User user1 = new User("Evgeniy", 39);
-//        User user2 = new User("Evgeniy Alekseevich Petrov", 23);
-        for (int i = 0; i < 100; i++) {
-            new Person(i, "N" + i);
-//            new User("Evgeniy", 39);
+        for (int i = 0; i < 1000; i++) {
+            new User("Evgeniy ", i);
         }
-        System.gc();
         info();
     }
 }
