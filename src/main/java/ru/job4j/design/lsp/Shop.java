e2a8cc5977bf12633220c25358fcfeab0083474c
CyberfuzZ <keepthelight@icloud.com>
2021-09-08 10:23:45 +0300

    1. Хранилище продуктов [#852 #200065]

--- src/main/java/ru/job4j/design/lsp/Shop.java
+++ src/main/java/ru/job4j/design/lsp/Shop.java
@@ -1,7 +1,5 @@
 package ru.job4j.design.lsp;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +8,7 @@ public class Shop implements Storage {
 
     @Override
     public boolean accept(Food food) {
-        double total = food.getCreateDate().until(food.getExpiryDate(), ChronoUnit.DAYS);
-        double passed = food.getCreateDate().until(LocalDate.now(), ChronoUnit.DAYS);
-        double percentPassed = passed / total * 100;
+        double percentPassed = FoodUtils.getPercentPassed(food);
         if (percentPassed >= 25 && percentPassed <= 75) {
             return true;
         } else if (percentPassed > 75 && percentPassed <= 100) {
--- src/test/java/ru/job4j/design/lsp/ControlQualityTest.java
+++ src/test/java/ru/job4j/design/lsp/ControlQualityTest.java
@@ -12,70 +12,66 @@ public class ControlQualityTest {
 
     @Test
     public void whenLess25PercentThenWarehouse() {
+        Storage trash = new Trash();
+        Storage shop = new Shop();
+        Storage warehouse = new Warehouse();
         List<Storage> storages = List.of(
-                new Warehouse(),
-                new Shop(),
-                new Trash()
+                trash, shop, warehouse
         );
         ControlQuality controlQuality = new ControlQuality(storages);
         LocalDate expiryDate = LocalDate.now().plusDays(80);
         LocalDate createDate = LocalDate.now().minusDays(20);
         Food food = new Milk("Milk", expiryDate, createDate, 50, 0);
         controlQuality.distribution(food);
-        Storage storage = storages.get(0);
-        assertThat(storage.getFoods(), is(List.of(food)));
-        assertThat(storage.getClass().getName(), is(Warehouse.class.getName()));
+        assertThat(warehouse.getFoods(), is(List.of(food)));
     }
 
     @Test
     public void whenBetween25And75PercentThenShop() {
+        Storage trash = new Trash();
+        Storage shop = new Shop();
+        Storage warehouse = new Warehouse();
         List<Storage> storages = List.of(
-                new Warehouse(),
-                new Shop(),
-                new Trash()
+                trash, shop, warehouse
         );
         ControlQuality controlQuality = new ControlQuality(storages);
         LocalDate expiryDate = LocalDate.now().plusDays(50);
         LocalDate createDate = LocalDate.now().minusDays(50);
         Food food = new Meat("Meat", expiryDate, createDate, 300, 0);
         controlQuality.distribution(food);
-        Storage storage = storages.get(1);
-        assertThat(storage.getFoods(), is(List.of(food)));
-        assertThat(storage.getClass().getName(), is(Shop.class.getName()));
+        assertThat(shop.getFoods(), is(List.of(food)));
     }
 
     @Test
     public void whenOver75PercentThenShopAndDiscount() {
+        Storage trash = new Trash();
+        Storage shop = new Shop();
+        Storage warehouse = new Warehouse();
         List<Storage> storages = List.of(
-                new Warehouse(),
-                new Shop(),
-                new Trash()
+                trash, shop, warehouse
         );
         ControlQuality controlQuality = new ControlQuality(storages);
         LocalDate expiryDate = LocalDate.now().plusDays(10);
         LocalDate createDate = LocalDate.now().minusDays(90);
         Food food = new Bread("Bread", expiryDate, createDate, 30, 0);
         controlQuality.distribution(food);
-        Storage storage = storages.get(1);
         assertThat(food.getDiscount(), is(50.0));
-        assertThat(storage.getFoods(), is(List.of(food)));
-        assertThat(storage.getClass().getName(), is(Shop.class.getName()));
+        assertThat(shop.getFoods(), is(List.of(food)));
     }
 
     @Test
     public void whenOver100PercentThenTrash() {
+        Storage trash = new Trash();
+        Storage shop = new Shop();
+        Storage warehouse = new Warehouse();
         List<Storage> storages = List.of(
-                new Warehouse(),
-                new Shop(),
-                new Trash()
+                trash, shop, warehouse
         );
         ControlQuality controlQuality = new ControlQuality(storages);
         LocalDate expiryDate = LocalDate.now().minusDays(10);
         LocalDate createDate = LocalDate.now().minusDays(100);
         Food food = new Orange("Orange", expiryDate, createDate, 40, 0);
         controlQuality.distribution(food);
-        Storage storage = storages.get(2);
-        assertThat(storage.getFoods(), is(List.of(food)));
-        assertThat(storage.getClass().getName(), is(Trash.class.getName()));
+        assertThat(trash.getFoods(), is(List.of(food)));
     }
 }
